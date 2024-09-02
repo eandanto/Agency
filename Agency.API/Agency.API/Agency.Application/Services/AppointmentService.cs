9b5f4b700b2485afc111673b7a8c455bd70d08b6
erlangga <erlangga@hotelsuplay.com>
2024-09-02 12:06:59 +0700

    bug fix

--- Agency.API/Agency.API/Agency.Application/Services/AppointmentService.cs
+++ Agency.API/Agency.API/Agency.Application/Services/AppointmentService.cs
@@ -42,12 +42,9 @@ namespace Agency.Application.Services
 
         public async Task<AppointmentListDto> GetAllAppointments(int pageNo, int pageSize, DateTime date)
         {
-            if (date.Kind != DateTimeKind.Utc)
-            {
-                date = date.Date;
-                date = DateTime.SpecifyKind(date, DateTimeKind.Local);
-                date = date.ToUniversalTime();
-            }
+            date = date.Date;
+            date = DateTime.SpecifyKind(date, DateTimeKind.Local);
+            date = date.ToUniversalTime();
 
             var myAppointments = await _appointmentRepository.GetAllAppointments(pageNo, pageSize, date);
             var totalCount = await _appointmentRepository.GetAllAppointmentsCount(date);
